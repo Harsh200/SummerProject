be8c4df8a28eac7d1ff6d3cc3ca79390843df039
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-19 15:21:36 +0530

    returning data.read

--- robots_txt.py
+++ robots_txt.py
@@ -4,3 +4,13 @@ import io
 
 #function to get robots.txt file
 def get_robots_txt(url):
+    if url.endswith('/'):
+        path=url
+    else:
+        path=url + "/"
+
+    req=urllib.request.urlopen(path + "robots.txt",data=None)
+
+    data=io.TextIOWrapper(req,encoding='utf-8')
+    print("Robots.txt done!")
+    return data.read()
