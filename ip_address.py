891f435a2a64c4f160ed19557b96b75809e13f9f
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-19 15:04:17 +0530

    end of get ip address function

--- ip_address.py
+++ ip_address.py
@@ -2,3 +2,7 @@ import os
 
 #get ip address module
 def get_ip_address(url):
+    command="host"+url
+    process=os.popen(command)
+    results=str(process.read())
+    marker=results.find('has address') + 12
