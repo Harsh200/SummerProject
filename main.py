578ec305cbf0c5a9ffab09f5f6b6b7ec3debb535
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-19 15:36:53 +0530

    assigning different variables info

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ from whois import *
 ROOT_DIR='companies'
 create_dir(ROOT_DIR)
 def gather_info(name,url):
+    robots_txt=get_robots_txt(url)
+    domain_name=get_domain_name(url)
+    whois=get_whois(domain_name)
+    ip_address=get_ip_address(domain_name)
+    nmap=get_nmap("-F",ip_address)
