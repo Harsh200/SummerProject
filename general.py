e0a4642ddf630668f1b7ea2577273c3dcad88451
Harsh kumar saxena <harsh.saxena_ccv17@gla.ac.in>
2019-07-19 14:38:21 +0530

    this is the function for writing data in file

--- general.py
+++ general.py
@@ -1,4 +1,7 @@
 import os
+#function for creating a new directory
 def create_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+# function for writing data to file
